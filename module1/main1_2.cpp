1caef660e2e6aa6dd815d661da4d6bf4e8180fc9
Alekhin Sergey <alekhin1991@gmail.com>
2020-10-08 00:58:37 +0300

    create m1_2

---
+++ module1/main1_2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+/* Вычислить площадь выпуклого n-угольника, заданного координатами своих вершин.
+ * Вначале вводится количество вершин, затем последовательно целочисленные координаты
+ * всех вершин в порядке обхода против часовой стрелки. n < 1000, координаты < 10000.
+ * Указание. Для вычисления площади n-угольника можно посчитать сумму ориентированных
+ * площадей трапеций под каждой стороной многоугольника.
+ * Требования: Координата должна быть реализована в виде структуры. */
+
+
+struct Point {
+  explicit Point(int x = 0, int y = 0)
+      : x(x), y(y) {
+  }
+  int x;
+  int y;
+};
+
+void run(std::istream &is, std::ostream &os) {
+  int count;
+  std::cin >> count;
+  auto *pointsArray = new Point[count];
+  for (int i = 0; i < count; ++i) {
+    std::cin >> pointsArray[i].x >> pointsArray[i].y;
+  }
+
+  double square = 0;
+  for (int i = 0; i < count; ++i) {
+    int j = (i + 1) % count;
+    square += (pointsArray[i].y + pointsArray[j].y) / 2.0 * (pointsArray[i].x - pointsArray[j].x);
+  }
+
+  std::cout << std::abs(square);
+
+  delete[] pointsArray;
+}
+
+int main() {
+  run(std::cin, std::cout);
+
+  return 0;
+}
